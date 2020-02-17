ec31a2d1ac77630c27985d842d229210d30ed0ae
German Hernandez <german220291@gmail.com>
2020-02-17 18:08:05 +0000

    Initial Commit

---
+++ EventFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EventFinder
+//
+//  Created by German Hernandez on 17/02/2020.
+//  Copyright © 2020 German Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
